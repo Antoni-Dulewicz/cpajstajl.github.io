d8f9e9473edf1476b558eb017227b0a4748a403e
Antoni-Dulewicz <dulewicz@student.agh.edu.pl>
2023-11-01 17:28:28 +0100

    Changed validation to about_us file

--- validate.js
+++ validate.js
@@ -1,38 +1,46 @@
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
+var nameError = document.getElementById('name_error');
+var emailError = document.getElementById('email_error');
+var submitError = document.getElementById('submit_error');
 
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
+function validateName(){
+    var name = document.getElementById('contact_name').value;
 
-email.addEventListener('input', email_Verify);
-password.addEventListener('input', pass_Verify);
-
-function validated(){
-    if (email.value.length < 9){
-        email_error.style.display = "block";
-        email.focus();
+    if(name.length == 0){
+        nameError.innerHTML = "Name is required";
         return false;
     }
 
-    if (password.value.length < 5){
-        pass_error.style.display = "block";
-        password.focus();
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Write full name";
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check" style="color: #1eca1c;"></i>';
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact_email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = "Email is required";
         return false;
     }
 
-    return true; // Form is valid
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = "Email invalid";
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check" style="color: #1eca1c;"></i>';
+    return true;
 }
 
-function email_Verify(){
-    if (email.value.length >= 8){
-        email_error.style.display = "none";
-        return true;
+function validateForm(){
+    if(!validateName() || !validateForm()){
+        submitError.style.display = 'block'
+        submitError.innerHTML = 'Please fix error';
+        setTimeout(function(){submitError.style.display = 'none'},2000)
+        return false;
     }
-}
 
-function pass_Verify(){
-    if (password.value.length >= 5){
-        pass_error.style.display = "none";
-        return true;
-    }
 }
